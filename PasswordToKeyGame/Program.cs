09a90fe890bbca84a7e3b3aa5537f1f6fb9089a2
Ruper Ottolenghi <ruper120406@gmail.com>
2023-03-14 10:56:01 +0200

    Seccessfuly modified  some functions into inner functions

--- PasswordToKeyGame/Program.cs
+++ PasswordToKeyGame/Program.cs
@@ -49,22 +49,6 @@ namespace PasswordToKeyGame
             ToPush = null;
             Reader = null;
         }
-        static void LeftArrow()
-        {
-            Reader = Backwards.Pop().ToString();
-
-            Forwards.Push(ToPush);
-
-            StackFunction();
-        }
-        static void RightArrow()
-        {
-            Reader = Forwards.Pop().ToString();
-
-            Backwards.Push(ToPush);
-
-            StackFunction();
-        }
         static void CheckMe()
         {
             if (Left)
@@ -172,6 +156,22 @@ namespace PasswordToKeyGame
                     Reader = ToPush;
                     StackFunction();
                 }
+            }
+            void LeftArrow()
+            {
+                Reader = Backwards.Pop().ToString();
+
+                Forwards.Push(ToPush);
+
+                StackFunction();
+            }
+            void RightArrow()
+            {
+                Reader = Forwards.Pop().ToString();
+
+                Backwards.Push(ToPush);
+
+                StackFunction();
             }
         }
         static void MainMenu()
@@ -287,7 +287,7 @@ namespace PasswordToKeyGame
                     CheckMe();
                     return;
                 }
-                if (!String.IsNullOrEmpty(NewUserName) && !UserNameClient(NewUserName.Trim( ), true))
+                if (!String.IsNullOrEmpty(NewUserName) && !UserNameClient(NewUserName.Trim(), true))
                     break;
             }
 
@@ -357,7 +357,7 @@ namespace PasswordToKeyGame
 ");
 
             X = Console.CursorLeft;
-            Y= Console.CursorTop;
+            Y = Console.CursorTop;
 
             while (NumberOfTimesHeGotTheUserNameWrong != 4)
             {
@@ -439,6 +439,173 @@ namespace PasswordToKeyGame
                     else
                         break;
                 }
+            }
+            bool Password(string UserName, string PasswordClient)
+            {
+                bool FoundIt = false;
+
+                ACCDB_Type_File($"SELECT UserName, Password FROM UserNameAndPassword WHERE UserName = '{UserName}' AND Password = '{PasswordClient}'", true, ref FoundIt);
+
+                if (FoundIt)
+                {
+                    Console.Write("\nyou may enter");
+
+                    Thread.Sleep(1000);
+
+                    for (int k = 0; k < 3; k++)
+                    {
+                        Console.Write(".");
+                        Thread.Sleep(1000);
+                    }
+
+                    Console.Clear();
+
+                    Console.ResetColor();
+
+                    MyKeyGame();
+                }
+
+                else
+                {
+                    Console.Write("\nyou've enterd the wrong Password");
+                    Thread.Sleep(2000);
+                }
+                return FoundIt;
+            }
+            void MyKeyGame()
+            {
+                Random random = new Random();
+
+                ConsoleKeyInfo KeyInfo;
+
+                bool FirstTime = true, Left = false, NewLine = false, Done = false, FinichLine = false;
+
+                string p = "*";
+
+                int[] Num = new int[2];
+
+                int x = 0, y = 0, w = 0, h = 1;
+
+                int Width, Height;
+
+                Width = Console.WindowWidth;
+
+                Height = Console.WindowHeight;
+
+                Display(0, 19, Num[0], "C - Clear Screan, P - Change pen, R - Change Color to a random Color, W - Change color to Wite, U - Pen Up, D - Pen Down,B - Carriage Return, Escape - Exit");
+
+                do
+                {
+                    if (NewLine)
+                        h = 1;
+
+                    if (Left)
+                        Display(x + h, y, Num[0], "");
+
+                    if (Done)
+                        Display(x + 1, y, Num[0], "");
+
+                    if (FinichLine)
+                    {
+                        Display(Width - 1, y, Num[0], "");
+                        x = Width - 2;
+                    }
+
+                    Done = false;
+
+                    Left = false;
+
+                    FinichLine = false;
+
+                    KeyInfo = Console.ReadKey();
+
+                    if (KeyInfo.Key == ConsoleKey.RightArrow)
+                    {
+                        if (!FirstTime)
+                            x++;
+                        if (x < Width - 1)
+                            Display(x, y, Num[0], p);
+                        else
+                            FinichLine = true;
+
+                        FirstTime = false;
+                    }
+
+                    else if (KeyInfo.Key == ConsoleKey.LeftArrow)
+                    {
+                        if (x > 0)
+                            h++;
+
+                        Left = true;
+
+                        if (x > 0)
+                            Display(--x, y, Num[0], p);
+                    }
+
+                    else if (KeyInfo.Key == ConsoleKey.DownArrow)
+                    {
+                        if (y < Height - 2)
+                            Display(x, ++y, Num[0], p);
+                        else
+                            Done = true;
+                    }
+
+                    else if (KeyInfo.Key == ConsoleKey.UpArrow)
+                    {
+                        if (y > 0)
+                            Display(x, --y, Num[0], p);
+                        else
+                            Done = true;
+                    }
+
+                    else if (KeyInfo.Key == ConsoleKey.B)
+                    {
+                        Display(0, y, Num[0], "");
+                        x = -1;
+                    }
+
+                    else if (KeyInfo.Key == ConsoleKey.C)
+                    {
+                        Console.Clear();
+                    }
+
+                    else if (KeyInfo.Key == ConsoleKey.P)
+                    {
+                        Num[1] = random.Next(33, 48);
+
+                        p = Convert.ToString(Convert.ToChar(Num[1]));
+                    }
+
+                    else if (KeyInfo.Key == ConsoleKey.R)
+                    {
+                        Num[0] = random.Next(1, 15);
+
+                        PaintTheSkeyInBlwo(Num[0], "");
+                    }
+
+                    else if (KeyInfo.Key == ConsoleKey.W)
+                    {
+                        Num[0] = 15;
+                        PaintTheSkeyInBlwo(Num[0], "");
+                    }
+
+                    else if (KeyInfo.Key == ConsoleKey.Escape)
+                    {
+                        Num[0] = 0;
+
+                        Display(0, 22, Num[0], "It was nice to have you here \n");
+                        break;
+                    }
+
+                    NewLine = false;
+
+                    if (y != w)
+                    {
+                        w = y;
+                        NewLine = true;
+                    }
+
+                } while (true);
             }
         }
         static void UpdateMenu()
@@ -554,38 +721,6 @@ namespace PasswordToKeyGame
             }
             return FoundIt;
         }
-        static bool Password(string UserName, string PasswordClient)
-        {
-            bool FoundIt = false;
-
-            ACCDB_Type_File($"SELECT UserName, Password FROM UserNameAndPassword WHERE UserName = '{UserName}' AND Password = '{PasswordClient}'", true, ref FoundIt);
-
-            if (FoundIt)
-            {
-                Console.Write("\nyou may enter");
-
-                Thread.Sleep(1000);
-
-                for (int k = 0; k < 3; k++)
-                {
-                    Console.Write(".");
-                    Thread.Sleep(1000);
-                }
-
-                Console.Clear();
-
-                Console.ResetColor();
-
-                MyKeyGame();
-            }
-
-            else
-            {
-                Console.Write("\nyou've enterd the wrong Password");
-                Thread.Sleep(2000);
-            }
-            return FoundIt;
-        }
         static void PaintTheSkeyInBlwo(int Num, string p)
         {
             if (Num != 0)
@@ -598,141 +733,6 @@ namespace PasswordToKeyGame
             Console.SetCursorPosition(x, y);
 
             PaintTheSkeyInBlwo(Num, p);
-        }
-        static void MyKeyGame()
-        {
-            Random random = new Random();
-
-            ConsoleKeyInfo KeyInfo;
-
-            bool FirstTime = true, Left = false, NewLine = false, Done = false, FinichLine = false;
-
-            string p = "*";
-
-            int[] Num = new int[2];
-
-            int x = 0, y = 0, w = 0, h = 1;
-
-            int Width, Height;
-
-            Width = Console.WindowWidth;
-
-            Height = Console.WindowHeight;
-
-            Display(0, 19, Num[0], "C - Clear Screan, P - Change pen, R - Change Color to a random Color, W - Change color to Wite, U - Pen Up, D - Pen Down,B - Carriage Return, Escape - Exit");
-
-            do
-            {
-                if (NewLine)
-                    h = 1;
-
-                if (Left)
-                    Display(x + h, y, Num[0], "");
-
-                if (Done)
-                    Display(x + 1, y, Num[0], "");
-
-                if (FinichLine)
-                {
-                    Display(Width - 1, y, Num[0], "");
-                    x = Width - 2;
-                }
-
-                Done = false;
-
-                Left = false;
-
-                FinichLine = false;
-
-                KeyInfo = Console.ReadKey();
-
-                if (KeyInfo.Key == ConsoleKey.RightArrow)
-                {
-                    if (!FirstTime)
-                        x++;
-                    if (x < Width - 1)
-                        Display(x, y, Num[0], p);
-                    else
-                        FinichLine = true;
-
-                    FirstTime = false;
-                }
-
-                else if (KeyInfo.Key == ConsoleKey.LeftArrow)
-                {
-                    if (x > 0)
-                        h++;
-
-                    Left = true;
-
-                    if (x > 0)
-                        Display(--x, y, Num[0], p);
-                }
-
-                else if (KeyInfo.Key == ConsoleKey.DownArrow)
-                {
-                    if (y < Height - 2)
-                        Display(x, ++y, Num[0], p);
-                    else
-                        Done = true;
-                }
-
-                else if (KeyInfo.Key == ConsoleKey.UpArrow)
-                {
-                    if (y > 0)
-                        Display(x, --y, Num[0], p);
-                    else
-                        Done = true;
-                }
-
-                else if (KeyInfo.Key == ConsoleKey.B)
-                {
-                    Display(0, y, Num[0], "");
-                    x = -1;
-                }
-
-                else if (KeyInfo.Key == ConsoleKey.C)
-                {
-                    Console.Clear();
-                }
-
-                else if (KeyInfo.Key == ConsoleKey.P)
-                {
-                    Num[1] = random.Next(33, 48);
-
-                    p = Convert.ToString(Convert.ToChar(Num[1]));
-                }
-
-                else if (KeyInfo.Key == ConsoleKey.R)
-                {
-                    Num[0] = random.Next(1, 15);
-
-                    PaintTheSkeyInBlwo(Num[0], "");
-                }
-
-                else if (KeyInfo.Key == ConsoleKey.W)
-                {
-                    Num[0] = 15;
-                    PaintTheSkeyInBlwo(Num[0], "");
-                }
-
-                else if (KeyInfo.Key == ConsoleKey.Escape)
-                {
-                    Num[0] = 0;
-
-                    Display(0, 22, Num[0], "It was nice to have you here \n");
-                    break;
-                }
-
-                NewLine = false;
-
-                if (y != w)
-                {
-                    w = y;
-                    NewLine = true;
-                }
-
-            } while (true);
         }
         static void Main(string[] args)
         {
